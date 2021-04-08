555ca7548694e7832846da441f4219a1b51f1c24
Jsubak <tnwjd1003@gmail.com>
2021-04-08 12:47:37 +0900

    마름모 만들기

---
+++ ch04-7-8.js
@@ -0,0 +1,28 @@
+let output = "";
+
+for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10-i; j++){
+        output += " ";  
+    }
+    for (let j = 1; j < i+1; j++) {
+        output += "*";
+    }
+    for (let j = 0; j< i+1; j++) {
+        output += "*";
+    } 
+    output += "\n"
+}
+for (let i = 0; i < 11; i++) {
+    for (let j = 0; j < i; j++){
+        output += " ";
+    }
+    for (let j = 0; j < 11-i ; j++) {
+        output += "*";
+    }
+    for (let j = 1; j < 11-i ; j++) {
+        output += "*";
+    }
+    output += "\n"
+}
+output += "\n";
+console.log(output);
